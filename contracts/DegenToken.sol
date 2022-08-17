f7a364596f1a398b205e584047e3821ad75e8aee
CodingCas <casweeno2000@gmail.com>
2022-08-17 01:56:17 +0100

    total supply removed

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -6,10 +6,6 @@ import "@openzeppelin/token/ERC20/ERC20.sol";
 import "@openzeppelin/access/Ownable.sol";
 
 contract DegenToken is ERC20, Ownable {
-    uint256 public constant tokenPerPost = 2 * 10 ** 18;
-
-    uint public constant maxTotalSupply = 10000000 * 10 ** 18;
-
     constructor() ERC20("Degen Gas", "DEG") {}
 
     function mint(uint _amount) internal {
