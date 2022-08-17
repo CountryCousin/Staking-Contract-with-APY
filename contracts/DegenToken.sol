71d583a4a4f3da989c2cb5c21f6c377666c5141d
CodingCas <casweeno2000@gmail.com>
2022-08-17 01:57:25 +0100

    Updated

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -6,6 +6,8 @@ import "@openzeppelin/token/ERC20/ERC20.sol";
 import "@openzeppelin/access/Ownable.sol";
 
 contract DegenToken is ERC20, Ownable {
+    uint public constant maxTotalSupply = 10000000 * 10 ** 18;
+
     constructor() ERC20("Degen Gas", "DEG") {}
 
     function mint(uint _amount) internal {
